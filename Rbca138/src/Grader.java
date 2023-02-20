096f4722328d978a1ea610562dc50709269505b7
bca <bca@Bcalab4-m09>
2023-02-20 10:35:11 +0530

    Pushing the code into GitHub

---
+++ Rbca138/src/Grader.java
@@ -0,0 +1,22 @@
+
+public class Grader {
+	
+	int total_marks;
+	String roll_num;
+
+	Grader(int total_marks, String roll_num)
+	{
+		this.total_marks=total_marks;
+		this.roll_num=roll_num;
+	}
+	
+	public static void main(String[] args)
+	{
+		Grader g=new Grader(70,"R/BCA-777");
+	
+	
+	}
+	
+	
+
+}
